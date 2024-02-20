ffb545534c5c81d7640c8cae7005f78b26cb1544
Jorge <jzaldivar@google.com>
2024-02-20 20:29:17 +0100

    Format + wrap duplicate code in function

--- libs/vertexai/tests/unit_tests/test_function_utils.py
+++ libs/vertexai/tests/unit_tests/test_function_utils.py
@@ -4,30 +4,29 @@ from langchain_google_vertexai.functions_utils import _format_tool_to_vertex_fun
 
 
 def test_format_tool_to_vertex_function():
-
     @tool
     def get_datetime() -> str:
-        """ Gets the current datetime
-        """
+        """Gets the current datetime"""
         import datetime
+
         return datetime.datetime.now().strftime("%Y-%m-%d")
-    
+
     schema = _format_tool_to_vertex_function(get_datetime)
 
     assert schema["name"] == "get_datetime"
-    assert schema["description"] ==  "get_datetime() -> str - Gets the current datetime"
+    assert schema["description"] == "get_datetime() -> str - Gets the current datetime"
     assert "parameters" in schema
-    assert len(schema["parameters"]["required"]) == 0
+    assert "required" not in schema["parameters"]
 
     @tool
     def sum_two_numbers(a: float, b: float) -> str:
-        """ Sum two numbers 'a' and 'b'.
+        """Sum two numbers 'a' and 'b'.
 
         Returns:
             a + b in string format
         """
         return str(a + b)
-    
+
     schema = _format_tool_to_vertex_function(sum_two_numbers)
 
     assert schema["name"] == "sum_two_numbers"
@@ -36,12 +35,11 @@ def test_format_tool_to_vertex_function():
 
     @tool
     def do_something_optional(a: float, b: float = 0) -> str:
-        """ Some description
-        """
+        """Some description"""
         return a + b
-    
+
     schema = _format_tool_to_vertex_function(do_something_optional)
-    
+
     assert schema["name"] == "do_something_optional"
     assert "parameters" in schema
     assert len(schema["parameters"]["required"]) == 1
