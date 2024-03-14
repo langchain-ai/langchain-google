2ec5279ff5779822336b987099cf899feb71317e
Leonid Kuligin <lkuligin@yandex.ru>
2024-03-14 17:20:59 +0100

    moving code

---
+++ libs/tools/tests/integration_tests/test_docai_warehoure_retriever.py
@@ -0,0 +1,24 @@
+"""Test Google Cloud Document AI Warehouse retriever."""
+import os
+
+from langchain_core.documents import Document
+
+from langchain_google_tools import DocumentAIWarehouseRetriever
+
+
+def test_google_documentai_warehoure_retriever() -> None:
+    """In order to run this test, you should provide a project_id and user_ldap.
+
+    Example:
+    export USER_LDAP=...
+    export PROJECT_NUMBER=...
+    """
+    project_number = os.environ["PROJECT_NUMBER"]
+    user_ldap = os.environ["USER_LDAP"]
+    docai_wh_retriever = DocumentAIWarehouseRetriever(project_number=project_number)
+    documents = docai_wh_retriever.get_relevant_documents(
+        "What are Alphabet's Other Bets?", user_ldap=user_ldap
+    )
+    assert len(documents) > 0
+    for doc in documents:
+        assert isinstance(doc, Document)
