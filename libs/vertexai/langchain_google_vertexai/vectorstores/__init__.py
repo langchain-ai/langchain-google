51bf9ef26f5ef8630ab8b85627cc2dce5e8b8614
jzaldi <jzaldi89@gmail.com>
2024-03-22 17:03:44 +0100

    [vectorstores] Add new VectorSearch class with DataStore backend (#82)
    * Add new VectorSearch class with datastore backend
    
    ---------
    
    Co-authored-by: Jorge <jzaldivar@google.com>

---
+++ libs/vertexai/langchain_google_vertexai/vectorstores/__init__.py
@@ -0,0 +1,11 @@
+from langchain_google_vertexai.vectorstores.vectorstores import (
+    VectorSearchVectorStore,
+    VectorSearchVectorStoreDatastore,
+    VectorSearchVectorStoreGCS,
+)
+
+__all__ = [
+    "VectorSearchVectorStore",
+    "VectorSearchVectorStoreDatastore",
+    "VectorSearchVectorStoreGCS",
+]
