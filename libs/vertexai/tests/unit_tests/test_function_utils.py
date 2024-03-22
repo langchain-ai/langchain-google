70b1a295231b5ba34a3f23e3d6af1e77036142e0
Alex Ostapenko <aostapenko@google.com>
2024-03-22 17:14:41 +0000

    fixed the way how function definitions is constructed

--- libs/vertexai/tests/unit_tests/test_function_utils.py
+++ libs/vertexai/tests/unit_tests/test_function_utils.py
@@ -1,6 +1,12 @@
+from typing import Optional, Union
+
+from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.tools import tool
 
-from langchain_google_vertexai.functions_utils import _format_tool_to_vertex_function
+from langchain_google_vertexai.functions_utils import (
+    _format_tool_to_vertex_function,
+    _get_parameters_from_schema,
+)
 
 
 def test_format_tool_to_vertex_function():
@@ -43,3 +49,31 @@ def test_format_tool_to_vertex_function():
     assert schema["name"] == "do_something_optional"
     assert "parameters" in schema
     assert len(schema["parameters"]["required"]) == 1
+
+
+def test_get_parameters_from_schema():
+    class A(BaseModel):
+        a1: Optional[int]
+
+    class B(BaseModel):
+        b1: Optional[A]
+        b2: int = Field(description="f2")
+        b3: Union[int, str]
+
+    schema = B.schema()
+    result = _get_parameters_from_schema(schema)
+    assert result["type"] == "object"
+    assert "required" in result
+    assert len(result["required"]) == 2
+
+    assert "properties" in result
+    assert "b1" in result["properties"]
+    assert "b2" in result["properties"]
+    assert "b3" in result["properties"]
+
+    assert result["properties"]["b1"]["type"] == "object"
+    assert "a1" in result["properties"]["b1"]["properties"]
+    assert "required" not in result["properties"]["b1"]
+    assert len(result["properties"]["b1"]["properties"]) == 1
+
+    assert "anyOf" in result["properties"]["b3"]
