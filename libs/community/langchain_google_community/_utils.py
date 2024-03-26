8a0d61685eefbdb5dfc3b9284e1175c41720d026
Leonid Kuligin <lkuligin@yandex.ru>
2024-03-26 15:29:54 +0100

    code migration to community (#88)

---
+++ libs/community/langchain_google_community/_utils.py
@@ -0,0 +1,41 @@
+"""Utilities to init Vertex AI."""
+
+from importlib import metadata
+from typing import Optional, Tuple
+
+from google.api_core.gapic_v1.client_info import ClientInfo
+
+
+def get_user_agent(module: Optional[str] = None) -> Tuple[str, str]:
+    r"""Returns a custom user agent header.
+
+    Args:
+        module (Optional[str]):
+            Optional. The module for a custom user agent header.
+    Returns:
+        Tuple[str, str]
+    """
+    try:
+        langchain_version = metadata.version("langchain")
+    except metadata.PackageNotFoundError:
+        langchain_version = "0.0.0"
+    client_library_version = (
+        f"{langchain_version}-{module}" if module else langchain_version
+    )
+    return client_library_version, f"langchain/{client_library_version}"
+
+
+def get_client_info(module: Optional[str] = None) -> "ClientInfo":
+    r"""Returns a client info object with a custom user agent header.
+
+    Args:
+        module (Optional[str]):
+            Optional. The module for a custom user agent header.
+    Returns:
+        google.api_core.gapic_v1.client_info.ClientInfo
+    """
+    client_library_version, user_agent = get_user_agent(module)
+    return ClientInfo(
+        client_library_version=client_library_version,
+        user_agent=user_agent,
+    )
