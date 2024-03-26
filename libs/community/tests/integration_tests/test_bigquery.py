fd111e0dbea594b17000d1926858edc76d909be5
Leonid Kuligin <lkuligin@yandex.ru>
2024-03-26 16:55:34 +0100

    migrating code from community

---
+++ libs/community/tests/integration_tests/test_bigquery.py
@@ -0,0 +1,38 @@
+from langchain_google_community.bigquery import BigQueryLoader
+
+
+def test_bigquery_loader_no_options() -> None:
+    loader = BigQueryLoader("SELECT 1 AS a, 2 AS b")
+    docs = loader.load()
+
+    assert len(docs) == 1
+    assert docs[0].page_content == "a: 1\nb: 2"
+    assert docs[0].metadata == {}
+
+
+def test_bigquery_loader_page_content_columns() -> None:
+    loader = BigQueryLoader(
+        "SELECT 1 AS a, 2 AS b UNION ALL SELECT 3 AS a, 4 AS b",
+        page_content_columns=["a"],
+    )
+    docs = loader.load()
+
+    assert len(docs) == 2
+    assert docs[0].page_content == "a: 1"
+    assert docs[0].metadata == {}
+
+    assert docs[1].page_content == "a: 3"
+    assert docs[1].metadata == {}
+
+
+def test_bigquery_loader_metadata_columns() -> None:
+    loader = BigQueryLoader(
+        "SELECT 1 AS a, 2 AS b",
+        page_content_columns=["a"],
+        metadata_columns=["b"],
+    )
+    docs = loader.load()
+
+    assert len(docs) == 1
+    assert docs[0].page_content == "a: 1"
+    assert docs[0].metadata == {"b": 2}
