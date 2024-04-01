7fd96bce3a0b4896e93e9223a498a3a07e74e4d6
Holt Skinner <holtskinner@google.com>
2024-04-01 13:55:33 -0500

    feat: Allow loading of `gcs_uri` in Chat Messages

--- libs/vertexai/langchain_google_vertexai/chat_models.py
+++ libs/vertexai/langchain_google_vertexai/chat_models.py
@@ -130,13 +130,17 @@ def _parse_chat_history_gemini(
             )
         if part["type"] == "text":
             return Part.from_text(part["text"])
-        elif part["type"] == "image_url":
+
+        if part["type"] == "image_url":
             path = part["image_url"]["url"]
             image_bytes = ImageBytesLoader(project=project).load_bytes(path)
             image = Image.from_bytes(image_bytes)
-        else:
-            raise ValueError("Only text and image_url types are supported!")
-        return Part.from_image(image)
+            return Part.from_image(image)
+
+        if part["type"] == "gcs_uri":
+            return Part.from_uri(uri=part["gcs_uri"], mime_type=part["mime_type"])
+
+        raise ValueError("Only text and image_url types are supported!")
 
     def _convert_to_parts(message: BaseMessage) -> List[Part]:
         raw_content = message.content
