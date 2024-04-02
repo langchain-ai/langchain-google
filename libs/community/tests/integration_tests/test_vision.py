a0d1f71239b001fa822b59fc79e5cfee0263ab4d
Leonid Kuligin <lkuligin@yandex.ru>
2024-04-02 10:10:33 +0200

    added image loader

--- libs/community/tests/integration_tests/test_vision.py
+++ libs/community/tests/integration_tests/test_vision.py
@@ -4,7 +4,7 @@ import pytest
 from langchain_core.document_loaders.blob_loaders import Blob
 from langchain_core.documents import Document
 
-from langchain_google_community import CloudVisionLoader
+from langchain_google_community import CloudVisionLoader, CloudVisionParser
 
 
 @pytest.mark.skip(reason="CI/CD not ready.")
@@ -12,8 +12,19 @@ def test_parse_image() -> None:
     gcs_path = os.environ["IMAGE_GCS_PATH"]
     project = os.environ["PROJECT"]
     blob = Blob(path=gcs_path, data="")  # type: ignore
-    loader = CloudVisionLoader(project=project)
+    loader = CloudVisionParser(project=project)
     documents = loader.parse(blob)
+    assert len(documents) == 1
+    assert isinstance(documents[0], Document)
+    assert len(documents[0].page_content) > 1
+
+
+@pytest.mark.skip(reason="CI/CD not ready.")
+def test_load_image() -> None:
+    gcs_path = os.environ["IMAGE_GCS_PATH"]
+    project = os.environ["PROJECT"]
+    loader = CloudVisionLoader(project=project, file_path=gcs_path)
+    documents = loader.load()
     assert len(documents) == 1
     assert isinstance(documents[0], Document)
     assert len(documents[0].page_content) > 1
