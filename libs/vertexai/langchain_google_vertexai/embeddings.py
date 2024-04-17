98123d327014cd7a13a88e768b0238f8921df7fe
eliasecchig <115624100+eliasecchig@users.noreply.github.com>
2024-04-17 12:20:18 +0000

    fix

--- libs/vertexai/langchain_google_vertexai/embeddings.py
+++ libs/vertexai/langchain_google_vertexai/embeddings.py
@@ -44,7 +44,7 @@ class GoogleEmbeddingModelType(str, Enum):
 
     @classmethod
     def _missing_(cls, value: Any) -> Optional["GoogleEmbeddingModelType"]:
-        if "textembedding-gecko" in value.lower() or "text-embedding" in value.lower():
+        if value.lower().startswith("text"):
             return GoogleEmbeddingModelType.TEXT
         elif "multimodalembedding" in value.lower():
             return GoogleEmbeddingModelType.MULTIMODAL
