b416b1f290ad37ec64fffa0b1d3759e6f165d239
Grigory <movsic@gmail.com>
2024-04-25 22:13:48 +0200

    fix tests

--- libs/community/tests/integration_tests/test_vertex_ai_search.py
+++ libs/community/tests/integration_tests/test_vertex_ai_search.py
@@ -11,6 +11,7 @@ export PROJECT_ID=... - set to your Google Cloud project ID
 export DATA_STORE_ID=... - the ID of the search engine to use for the test
 """
 
+import os
 import pytest
 from langchain_core.documents import Document
 
@@ -23,7 +24,8 @@ from langchain_google_community import (
 
 def test_google_vertex_ai_search_get_relevant_documents() -> None:
     """Test the get_relevant_documents() method."""
-    retriever = VertexAIMultiTurnSearchRetriever()
+    google_cse_id = os.environ["GOOGLE_CSE_ID"]
+    retriever = VertexAIMultiTurnSearchRetriever(data_store_id=google_cse_id)
     documents = retriever.get_relevant_documents("What are Alphabet's Other Bets?")
     assert len(documents) > 0
     for doc in documents:
@@ -35,7 +37,8 @@ def test_google_vertex_ai_search_get_relevant_documents() -> None:
 
 def test_google_vertex_ai_multiturnsearch_get_relevant_documents() -> None:
     """Test the get_relevant_documents() method."""
-    retriever = VertexAISearchRetriever()
+    google_cse_id = os.environ["GOOGLE_CSE_ID"]
+    retriever = VertexAISearchRetriever(data_store_id=google_cse_id)
     documents = retriever.get_relevant_documents("What are Alphabet's Other Bets?")
     assert len(documents) > 0
     for doc in documents:
