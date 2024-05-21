7cbf259f36a3f247d1cfcd8aa1b9fa887e468069
Alex Ostapenko <aostapenko@google.com>
2024-05-21 11:41:28 +0000

    fixed forced tool calls on gemini-preview models

---
+++ libs/vertexai/tests/unit_tests/test_utils.py
@@ -0,0 +1,47 @@
+from typing import List
+
+import pytest
+
+from langchain_google_vertexai._utils import GoogleModelFamily
+
+
+@pytest.mark.parametrize(
+    "srcs,exp",
+    [
+        (
+            [
+                "chat-bison@001",
+                "text-bison@002",
+            ],
+            GoogleModelFamily.PALM,
+        ),
+        (
+            [
+                "code-bison@002",
+                "code-gecko@002",
+            ],
+            GoogleModelFamily.CODEY,
+        ),
+        (
+            [
+                "gemini-1.0-pro-001",
+                "gemini-1.0-pro-002",
+                "gemini-1.0-pro-vision-001",
+                "gemini-1.0-pro-vision",
+            ],
+            GoogleModelFamily.GEMINI,
+        ),
+        (
+            [
+                "gemini-1.5-flash-preview-0514",
+                "gemini-1.5-pro-preview-0514",
+                "gemini-1.5-pro-preview-0409",
+            ],
+            GoogleModelFamily.GEMINI_ADVANCED,
+        ),
+    ],
+)
+def test_google_model_family(srcs: List[str], exp: GoogleModelFamily):
+    for src in srcs:
+        res = GoogleModelFamily(src)
+        assert res == exp
