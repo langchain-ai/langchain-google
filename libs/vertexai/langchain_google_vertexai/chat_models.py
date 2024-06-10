dbd1f55e2ffb45c306f3e3fae9ab7f5803b1558b
Nicolas Suzor <nic@suzor.com>
2024-06-11 03:25:00 +1000

    add response_mime_type for gemini models (allow JSON-mode output, optionally) (#228)
    * add response_mime_type to generation_config (optional)
    
    ---------
    
    Co-authored-by: Leonid Kuligin <lkuligin@yandex.ru>

--- libs/vertexai/langchain_google_vertexai/chat_models.py
+++ libs/vertexai/langchain_google_vertexai/chat_models.py
@@ -619,6 +619,14 @@ class ChatVertexAI(_VertexAICommon, BaseChatModel):
     """[Deprecated] Since new Gemini models support setting a System Message,
     setting this parameter to True is discouraged.
     """
+    response_mime_type: Optional[str] = None
+    """Optional. Output response mimetype of the generated candidate text. Only 
+        supported in Gemini 1.5 and later models. Supported mimetype: 
+            * "text/plain": (default) Text output. 
+            * "application/json": JSON response in the candidates.
+       The model also needs to be prompted to output the appropriate response 
+       type, otherwise the behavior is undefined. This is a preview feature.
+    """
 
     def __init__(self, *, model_name: Optional[str] = None, **kwargs: Any) -> None:
         """Needed for mypy typing to recognize model_name as a valid arg."""
@@ -688,6 +696,13 @@ class ChatVertexAI(_VertexAICommon, BaseChatModel):
     @property
     def _is_gemini_advanced(self) -> bool:
         return self.model_family == GoogleModelFamily.GEMINI_ADVANCED
+
+    @property
+    def _default_params(self) -> Dict[str, Any]:
+        updated_params = super()._default_params
+        if self.response_mime_type is not None:
+            updated_params["response_mime_type"] = self.response_mime_type
+        return updated_params
 
     def _get_ls_params(
         self, stop: Optional[List[str]] = None, **kwargs: Any
