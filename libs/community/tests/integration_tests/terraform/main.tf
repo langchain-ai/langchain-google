61f9d093064f298906f1654d3b5b124c42a11ab5
Grigory Movsesyan <mogr@google.com>
2024-06-24 13:56:19 +0200

    Update

--- libs/community/tests/integration_tests/terraform/main.tf
+++ libs/community/tests/integration_tests/terraform/main.tf
@@ -5,8 +5,8 @@ module "cloudbuild" {
   project_id                 = ""
   cloudbuildv2_repository_id = ""
   cloudbuild_env_vars = {
-    _DATA_STORE_ID = ""
-    _IMAGE_GCS_PATH = "gs://cloud-samples-data/vision/label/wakeupcat.jpg"
+    DATA_STORE_ID = ""
+    IMAGE_GCS_PATH = "gs://cloud-samples-data/vision/label/wakeupcat.jpg"
   }
   cloudbuild_secret_vars = {
     GOOGLE_API_KEY = ""
