04cff738f964c7f1b722434e5093553ff98da75f
Jorge <jzaldivar@google.com>
2024-07-02 13:38:36 +0200

    Add docstring and validation errors

--- libs/vertexai/langchain_google_vertexai/chat_models.py
+++ libs/vertexai/langchain_google_vertexai/chat_models.py
@@ -986,6 +986,10 @@ class ChatVertexAI(_VertexAICommon, BaseChatModel):
     """
 
     response_schema: Optional[Dict[str, Any]] = None
+    """ Optional. Enforce an schema to the output. Only works when `response_mime_type`
+        is set to `application/json`.
+        The format of the dictionary should follow Open API schema.
+    """
 
 
     def __init__(self, *, model_name: Optional[str] = None, **kwargs: Any) -> None:
@@ -1059,14 +1063,24 @@ class ChatVertexAI(_VertexAICommon, BaseChatModel):
 
     @property
     def _default_params(self) -> Dict[str, Any]:
+
         updated_params = super()._default_params
+
         if self.response_mime_type is not None:
             updated_params["response_mime_type"] = self.response_mime_type
+
         if self.response_schema is not None:
+
+            if self.response_mime_type != "application/json":
+                error_message = "`response_schema` is only supported when "
+                "`response_mime_type` is set to `application/json`."
+                raise ValueError(error_message)
+
             gapic_response_schema = _convert_schema_dict_to_gapic(
                 self.response_schema
             )
             updated_params["response_schema"] = gapic_response_schema
+            
         return updated_params
 
     def _get_ls_params(
