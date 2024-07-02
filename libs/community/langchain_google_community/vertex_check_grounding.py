927003c46ddf3b37fa2ca6b0b68ec05389440f60
Abhishek Bhagwat <abhishekbhgwt@google.com>
2024-07-02 14:57:43 +0800

    add client opts

--- libs/community/langchain_google_community/vertex_check_grounding.py
+++ libs/community/langchain_google_community/vertex_check_grounding.py
@@ -5,6 +5,7 @@ from google.auth.credentials import Credentials  # type: ignore
 from langchain_core.documents import Document
 from langchain_core.pydantic_v1 import BaseModel, Extra, Field
 from langchain_core.runnables import RunnableConfig, RunnableSerializable
+from langchain_google_community._utils import get_client_info
 
 if TYPE_CHECKING:
     from google.cloud import discoveryengine_v1alpha  # type: ignore
@@ -88,7 +89,8 @@ class VertexAICheckGroundingWrapper(
                 or Credentials.from_service_account_file(self.credentials_path)  # type: ignore[attr-defined]
                 if self.credentials_path
                 else None
-            )
+            ),
+            client_info=get_client_info(module="vertex-ai-search")
         )
 
     def invoke(
