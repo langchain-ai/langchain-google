7b038b408ff9528a46ed3881486826880e920717
Philippe Prados <github@prados.fr>
2024-07-30 08:50:54 +0200

    Add mock in unittest

--- libs/vertexai/tests/unit_tests/test_llm.py
+++ libs/vertexai/tests/unit_tests/test_llm.py
@@ -2,6 +2,8 @@ from typing import Any, Dict
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
+from langchain_core.pydantic_v1 import root_validator
+
 from langchain_google_vertexai._base import _BaseVertexAIModelGarden
 from langchain_google_vertexai.llms import VertexAI
 
@@ -77,6 +79,11 @@ def test_vertexai_args_passed() -> None:
 
 
 def test_extract_response() -> None:
+    class FakeModelGarden(_BaseVertexAIModelGarden):
+        @root_validator()
+        def validate_environment(cls, values: Dict) -> Dict:
+            return values
+
     prompts_results = [
         ("a prediction", "a prediction"),
         ("Prompt:\na prompt\nOutput:\na prediction", "a prediction"),
@@ -86,12 +93,12 @@ def test_extract_response() -> None:
         ),
         ("Prompt:\na prompt\nNo Output", "Prompt:\na prompt\nNo Output"),
     ]
-    model = _BaseVertexAIModelGarden(endpoint_id="123", result_arg="result")
+    model = FakeModelGarden(endpoint_id="123", result_arg="result", credentials="Fake")
     for original_result, result in prompts_results:
         assert model._parse_prediction(original_result) == result
         assert model._parse_prediction({"result": original_result}) == result
 
-    model = _BaseVertexAIModelGarden(endpoint_id="123", result_arg=None)
+    model = FakeModelGarden(endpoint_id="123", result_arg=None)
 
     class MyResult:
         def __init__(self, result):
