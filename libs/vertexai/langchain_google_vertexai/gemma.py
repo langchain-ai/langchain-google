af724cd1fd7f7a83277154cda301cfe8389b3b3a
Sam <samhk1807@gmail.com>
2024-07-31 10:58:03 +0800

    Update gemma.py
    Fix model_name cannot pass to class _GemmaLocalHFBase and fix the default model name

--- libs/vertexai/langchain_google_vertexai/gemma.py
+++ libs/vertexai/langchain_google_vertexai/gemma.py
@@ -193,7 +193,7 @@ class _GemmaLocalKaggleBase(_GemmaBase):
 
     client: Any = None  #: :meta private:
     keras_backend: str = "jax"
-    model_name: str = Field(default="gemma_2b_en", alias="model")
+    model_name: str = Field(default="google/gemma_2b", alias="model")
     """Gemma model name."""
 
     def __init__(self, *, model_name: Optional[str] = None, **kwargs: Any) -> None:
@@ -301,8 +301,13 @@ class _GemmaLocalHFBase(_GemmaBase):
     client: Any = None  #: :meta private:
     hf_access_token: str
     cache_dir: Optional[str] = None
-    model_name: str = Field(default="gemma_2b_en", alias="model")
+    model_name: str = Field(default="google/gemma_2b", alias="model")
     """Gemma model name."""
+
+    class Config:
+        """Configuration for this pydantic object."""
+
+        allow_population_by_field_name = True
 
     @root_validator()
     def validate_environment(cls, values: Dict) -> Dict:
