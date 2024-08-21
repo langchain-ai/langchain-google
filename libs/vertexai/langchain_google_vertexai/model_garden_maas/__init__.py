85e361ab023b45eec219b410634b7124568c523f
Leonid Kuligin <lkuligin@yandex.ru>
2024-08-21 16:24:21 +0200

    fix after review

--- libs/vertexai/langchain_google_vertexai/model_garden_maas/__init__.py
+++ libs/vertexai/langchain_google_vertexai/model_garden_maas/__init__.py
@@ -5,6 +5,7 @@ _MISTRAL_MODELS = [
     "mistral-large@2407",
 ]
 _LLAMA_MODELS = ["meta/llama3-405b-instruct-maas"]
+_MAAS_MODELS = _MISTRAL_MODELS + _LLAMA_MODELS
 
 
 def get_vertex_maas_model(model_name, **kwargs):
@@ -12,7 +13,7 @@ def get_vertex_maas_model(model_name, **kwargs):
 
     A factory method based on model's name.
     """
-    if model_name not in _MISTRAL_MODELS + _LLAMA_MODELS:
+    if model_name not in _MAAS_MODELS:
         raise ValueError(f"model name {model_name} is not supported!")
     if model_name in _MISTRAL_MODELS:
         from langchain_google_vertexai.model_garden_maas.mistral import (  # noqa: F401
