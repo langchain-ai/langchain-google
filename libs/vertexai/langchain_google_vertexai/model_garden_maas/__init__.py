0d17477d034edc5b0c5a0bce3b94f9d46d234f3b
Leonid Kuligin <lkuligin@yandex.ru>
2024-09-17 09:05:12 +0200

    added smaller Llama models (#493)

--- libs/vertexai/langchain_google_vertexai/model_garden_maas/__init__.py
+++ libs/vertexai/langchain_google_vertexai/model_garden_maas/__init__.py
@@ -1,10 +1,9 @@
+from langchain_google_vertexai.model_garden_maas._base import (
+    _LLAMA_MODELS,
+    _MISTRAL_MODELS,
+)
 from langchain_google_vertexai.model_garden_maas.llama import VertexModelGardenLlama
 
-_MISTRAL_MODELS = [
-    "mistral-nemo@2407",
-    "mistral-large@2407",
-]
-_LLAMA_MODELS = ["meta/llama3-405b-instruct-maas"]
 _MAAS_MODELS = _MISTRAL_MODELS + _LLAMA_MODELS
 
 
