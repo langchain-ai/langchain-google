cc9c7b9d68fb092b885f8eab0e2e87b22ec223ce
Jorge <jzaldivar@google.com>
2024-10-04 22:34:17 +0200

    Fix format

--- libs/vertexai/langchain_google_vertexai/chat_models.py
+++ libs/vertexai/langchain_google_vertexai/chat_models.py
@@ -482,14 +482,14 @@ def _get_question(messages: List[BaseMessage]) -> HumanMessage:
 @overload
 def _parse_response_candidate(
     response_candidate: "Candidate", streaming: Literal[False] = False
-) -> AIMessage: 
+) -> AIMessage:
     ...
 
 
 @overload
 def _parse_response_candidate(
     response_candidate: "Candidate", streaming: Literal[True]
-) -> AIMessageChunk: 
+) -> AIMessageChunk:
     ...
 
 
