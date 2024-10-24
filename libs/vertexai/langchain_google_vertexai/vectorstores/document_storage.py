6c7bcd1ea9ca8ce71560c2ea0bccb86d28926cf4
Lorenzo Spataro <lspataro@google.com>
2024-10-24 18:36:27 +0200

    fixed linting errors and passed list exclude_from_indexes to exclude for indexing

--- libs/vertexai/langchain_google_vertexai/vectorstores/document_storage.py
+++ libs/vertexai/langchain_google_vertexai/vectorstores/document_storage.py
@@ -234,6 +234,7 @@ class DataStoreDocumentStorage(DocumentStorage):
         kind: str = "document_id",
         text_property_name: str = "text",
         metadata_property_name: str = "metadata",
+        exclude_from_indexes: List[str] = [],
     ) -> None:
         """Constructor.
         Args:
@@ -244,6 +245,7 @@ class DataStoreDocumentStorage(DocumentStorage):
         self._client = datastore_client
         self._text_property_name = text_property_name
         self._metadata_property_name = metadata_property_name
+        self.exclude_from_indexes = exclude_from_indexes
         self._kind = kind
 
     def mget(self, keys: Sequence[str]) -> List[Optional[Document]]:
@@ -290,8 +292,7 @@ class DataStoreDocumentStorage(DocumentStorage):
             entities = []
             for key, document in zip(keys, documents):
                 entity = self._client.entity(
-                    key=key,
-                    exclude_from_indexes=[self._text_property_name]
+                    key=key, exclude_from_indexes=self.exclude_from_indexes
                 )
                 entity[self._text_property_name] = document.page_content
                 entity[self._metadata_property_name] = document.metadata
