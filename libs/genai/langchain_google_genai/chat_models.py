019d57874966e6d8692dd1d321d9c7f721eec4ea
Yelin Zhang <30687616+Yelinz@users.noreply.github.com>
2024-12-03 07:45:16 +0100

    docs: fix google_genai typos (#614)
    Co-authored-by: Leonid Kuligin <lkuligin@yandex.ru>

--- libs/genai/langchain_google_genai/chat_models.py
+++ libs/genai/langchain_google_genai/chat_models.py
@@ -579,9 +579,9 @@ class ChatGoogleGenerativeAI(_BaseGoogleGenerativeAI, BaseChatModel):
     Instantiation:
         To use, you must have either:
 
-            1. The ``GOOGLE_API_KEY``` environment variable set with your API key, or
-            2. Pass your API key using the google_api_key kwarg to the ChatGoogle
-               constructor.
+            1. The ``GOOGLE_API_KEY`` environment variable set with your API key, or
+            2. Pass your API key using the google_api_key kwarg
+            to the ChatGoogleGenerativeAI constructor.
 
         .. code-block:: python
 
