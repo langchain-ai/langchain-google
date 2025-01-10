975c9c399de4b99d8e5eca810320f7dd49b17617
Saurav Prateek <sauravprateek@google.com>
2025-01-10 14:25:11 +0530

    Added more documentation for integrated models in Model Garden

--- libs/vertexai/langchain_google_vertexai/__init__.py
+++ libs/vertexai/langchain_google_vertexai/__init__.py
@@ -13,6 +13,8 @@ pip install -U langchain-google-vertexai
 
 1. Llama 
 2. Mistral
+3. Gemma
+4. Vision models
 
 Integration on Google Cloud Vertex AI Model-as-a-Service.
 
@@ -42,6 +44,75 @@ For more information, see:
 https://cloud.google.com/docs/authentication/application-default-credentials#GAC
 and 
 https://googleapis.dev/python/google-auth/latest/reference/google.auth.html#module-google.auth.
+
+## Chat Language Model vs Base Language Models
+
+A "base model" is a large language model trained on a wide variety of text data, 
+providing a general-purpose foundation for various tasks.
+
+While a "chat model" is a specialized version of a base model specifically fine-tuned 
+to excel in conversational interactions, meaning it's optimized to understand and 
+respond to dialogue within a chat context, often with features like maintaining 
+conversational context and generating engaging responses.
+
+## Gemma models
+
+Gemma is a family of lightweight, state-of-the-art open models built from the same 
+research and technology used to create the Gemini models. Developed by Google DeepMind 
+and other teams across Google.
+
+We currently support the following Gemma models.
+
+1. GemmaChatLocalHF: Gemma Chat local model from Hugging Face.
+2. GemmaChatLocalKaggle - Gemma chat local model from Kaggle.
+3. GemmaLocalHF - Gemma Local model from Hugging Face
+4. GemmaLocalKaggle - Gemma Local model from Kaggle
+
+## Vision models
+
+### Image Captioning
+
+Generate Captions from Image. Implementation of the Image Captioning model 
+
+The model takes a list of prompts as an input where each prompt must be a 
+string that represents an image. Currently supported are:
+
+1. Google Cloud Storage URI 
+2. B64 encoded string
+3. Local file path
+4. Remote url
+
+The model returns Captions generated from every prompt (Image).
+
+### Image Generation
+
+Generates images from text prompt. 
+
+The message must be a list of only one element with one part i.e. the user prompt.
+
+### Visual QnA Chat
+
+Answers questions about an image. Chat implementation of a visual QnA model.
+
+The model takes a list of messages. The first message should contain a string 
+representation of the image. Currently supported are:
+
+1. Google Cloud Storage URI
+2. B64 encoded string
+3. Local file path
+4. Remote url
+
+There has to be at least another message with the first question.
+The model returns the generated answer for the questions asked.
+
+### Image Editor
+
+Given an image and a prompt, edit the image. Currently only supports mask free editing.
+
+The message must be a list of only one element with two part:
+
+1. The image as a dictionary: { 'type': 'image_url', 'image_url': {'url': <message_str>} }
+2. The user prompt.
 
 ## Chat Models
 
