aeba9f0e64a7a4c26433e0e35ad17957caaa5b77
Leonid Kuligin <lkuligin@yandex.ru>
2025-01-23 20:42:19 +0100

    fix linting

--- libs/vertexai/langchain_google_vertexai/_base.py
+++ libs/vertexai/langchain_google_vertexai/_base.py
@@ -177,7 +177,7 @@ class _VertexAIBase(BaseModel):
 
 class _VertexAICommon(_VertexAIBase):
     client_preview: Any = Field(default=None, exclude=True)  #: :meta private:
-    model_name: str = Field(default=None, alias="model")
+    model_name: Optional[str] = Field(default=None, alias="model")
     "Underlying model name."
     temperature: Optional[float] = None
     "Sampling temperature, it controls the degree of randomness in token selection."
