d746ded74dafbfc1c2c859536db47839d990e888
Leonid Kuligin <lkuligin@yandex.ru>
2025-01-24 16:29:09 +0100

    fix tests

--- libs/vertexai/tests/unit_tests/test_standard.py
+++ libs/vertexai/tests/unit_tests/test_standard.py
@@ -6,16 +6,6 @@ from langchain_tests.unit_tests import ChatModelUnitTests
 from langchain_google_vertexai import ChatVertexAI
 
 
-class TestGeminiAIStandard(ChatModelUnitTests):
-    @property
-    def chat_model_class(self) -> Type[BaseChatModel]:
-        return ChatVertexAI
-
-    @property
-    def chat_model_params(self) -> dict:
-        return {"model_name": "gemini-1.0-pro-001"}
-
-
 class TestGemini_15_AIStandard(ChatModelUnitTests):
     @property
     def chat_model_class(self) -> Type[BaseChatModel]:
