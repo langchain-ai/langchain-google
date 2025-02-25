6e9fa5fa8a466c793979f1e5adb4442b82dbe053
Heng GAO <heng.gao.us@gmail.com>
2025-02-24 22:19:00 -0600

    revert formatting

--- libs/genai/langchain_google_genai/_function_utils.py
+++ libs/genai/langchain_google_genai/_function_utils.py
@@ -184,7 +184,9 @@ def _format_to_gapic_function_declaration(
         ):
             function = cast(dict, tool)
         else:
-            if "parameters" in tool and tool["parameters"].get("properties"):  # type: ignore[index]
+            if (
+                "parameters" in tool and tool["parameters"].get("properties")  # type: ignore[index]
+            ):
                 function = convert_to_openai_tool(cast(dict, tool))["function"]
             else:
                 function = cast(dict, tool)
