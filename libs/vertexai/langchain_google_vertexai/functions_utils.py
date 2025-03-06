24db7d21f31c99b51deac8833aaffc78d4c2c14e
Leonid Kuligin <lkuligin@yandex.ru>
2025-03-06 17:52:46 +0100

    added endpoint_version

--- libs/vertexai/langchain_google_vertexai/functions_utils.py
+++ libs/vertexai/langchain_google_vertexai/functions_utils.py
@@ -18,6 +18,9 @@ from typing import (
 
 import google.cloud.aiplatform_v1beta1.types as gapic
 import vertexai.generative_models as vertexai  # type: ignore
+from google.cloud.aiplatform_v1beta1.types import (
+    ToolConfig as GapicToolConfig,
+)
 from langchain_core.exceptions import OutputParserException
 from langchain_core.output_parsers import BaseOutputParser
 from langchain_core.outputs import ChatGeneration, Generation
@@ -424,7 +427,7 @@ def _format_tool_config(tool_config: _ToolConfigDict) -> Union[gapic.ToolConfig,
 def _tool_choice_to_tool_config(
     tool_choice: _ToolChoiceType,
     all_names: List[str],
-) -> _ToolConfigDict:
+) -> Optional[GapicToolConfig]:
     allowed_function_names: Optional[List[str]] = None
     if tool_choice is True or tool_choice == "any":
         mode = gapic.FunctionCallingConfig.Mode.ANY
@@ -455,9 +458,10 @@ def _tool_choice_to_tool_config(
             )
     else:
         raise ValueError(f"Unrecognized tool choice format:\n\n{tool_choice=}")
-    return _ToolConfigDict(
+    tool_config = _ToolConfigDict(
         function_calling_config=_FunctionCallingConfigDict(
             mode=mode,
             allowed_function_names=allowed_function_names,
         )
     )
+    return _format_tool_config(tool_config)
