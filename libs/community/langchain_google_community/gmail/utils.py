1ab3ce6006771bf962e63bb9879eac0ab97c3e62
CtrlMj <43945314+CtrlMj@users.noreply.github.com>
2025-05-07 16:15:06 -0400

    community: Unify google service authentications and move to parent's _utils file (#904)

--- libs/community/langchain_google_community/gmail/utils.py
+++ libs/community/langchain_google_community/gmail/utils.py
@@ -3,128 +3,78 @@
 from __future__ import annotations
 
 import logging
-import os
-from typing import TYPE_CHECKING, List, Optional, Tuple
+import warnings
+from typing import TYPE_CHECKING, List, Optional
+
+from langchain_google_community._utils import (
+    get_google_credentials,
+    import_googleapiclient_resource_builder,
+)
 
 if TYPE_CHECKING:
-    from google.auth.transport.requests import Request  # type: ignore[import]
     from google.oauth2.credentials import Credentials  # type: ignore[import]
-    from google.oauth2.service_account import Credentials as ServiceCredentials
-    from google_auth_oauthlib.flow import InstalledAppFlow  # type: ignore[import]
     from googleapiclient.discovery import Resource  # type: ignore[import]
-    from googleapiclient.discovery import build as build_resource
 
 logger = logging.getLogger(__name__)
 
 
-def import_google() -> Tuple[Request, Credentials, ServiceCredentials]:
-    """Import google libraries.
-
-    Returns:
-        Tuple[Request, Credentials]: Request and Credentials classes.
-    """
-    try:
-        from google.auth.transport.requests import Request  # noqa: F401
-        from google.oauth2.credentials import Credentials  # noqa: F401
-        from google.oauth2.service_account import Credentials as ServiceCredentials
-    except ImportError:
-        raise ImportError(
-            "You need to install gmail dependencies to use this toolkit. "
-            "Try running poetry install --with gmail"
-        )
-    return Request, Credentials, ServiceCredentials  # type: ignore[return-value]
-
-
-def import_installed_app_flow() -> InstalledAppFlow:
-    """Import InstalledAppFlow class.
-
-    Returns:
-        InstalledAppFlow: InstalledAppFlow class.
-    """
-    try:
-        from google_auth_oauthlib.flow import InstalledAppFlow
-    except ImportError:
-        raise ImportError(
-            "You need to install gmail dependencies to use this toolkit. "
-            "Please, install bigquery dependency group: "
-            "`pip install langchain-google-community[gmail]`"
-        )
-    return InstalledAppFlow
-
-
-def import_googleapiclient_resource_builder() -> build_resource:
-    """Import googleapiclient.discovery.build function.
-
-    Returns:
-        build_resource: googleapiclient.discovery.build function.
-    """
-    try:
-        from googleapiclient.discovery import build
-    except ImportError:
-        raise ImportError(
-            "You need to install all dependencies to use this toolkit. "
-            "Try running pip install langchain-google-community"
-        )
-    return build
-
-
 DEFAULT_SCOPES = ["https://mail.google.com/"]
 DEFAULT_SERVICE_SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
-DEFAULT_CREDS_TOKEN_FILE = "token.json"
-DEFAULT_CLIENT_SECRETS_FILE = "credentials.json"
-DEFAULT_SERVICE_ACCOUNT_FILE = "service_account.json"
 
 
 def get_gmail_credentials(
     token_file: Optional[str] = None,
-    client_secrets_file: Optional[str] = None,
+    client_sercret_file: Optional[str] = None,
     service_account_file: Optional[str] = None,
     scopes: Optional[List[str]] = None,
     use_domain_wide: bool = False,
     delegated_user: Optional[str] = None,
 ) -> Credentials:
-    """Get credentials."""
+    """Get Gmail credentials."""
+    warnings.warn(
+        "get_gmail_credentials is deprecated and will be removed in a future version."
+        "Use get_google_credentials instead.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
     if use_domain_wide:
-        _, _, ServiceCredentials = import_google()
-        service_account_file = service_account_file or DEFAULT_SERVICE_ACCOUNT_FILE
         scopes = scopes or DEFAULT_SERVICE_SCOPES
-        credentials = ServiceCredentials.from_service_account_file(
-            service_account_file, scopes=scopes
-        )
-
-        if delegated_user:
-            credentials = credentials.with_subject(delegated_user)
-
-        return credentials
     else:
-        # From https://developers.google.com/gmail/api/quickstart/python
-        Request, Credentials, _ = import_google()
-        InstalledAppFlow = import_installed_app_flow()
-        creds = None
         scopes = scopes or DEFAULT_SCOPES
-        token_file = token_file or DEFAULT_CREDS_TOKEN_FILE
-        client_secrets_file = client_secrets_file or DEFAULT_CLIENT_SECRETS_FILE
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
 
-        if os.path.exists(token_file):
-            creds = Credentials.from_authorized_user_file(token_file, scopes)
+    return get_google_credentials(
+        scopes=scopes,
+        token_file=token_file,
+        client_secrets_file=client_sercret_file,
+        service_account_file=service_account_file,
+        use_domain_wide=use_domain_wide,
+        delegated_user=delegated_user,
+    )
 
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())  # type: ignore[call-arg]
-            else:
-                # https://developers.google.com/gmail/api/quickstart/python#authorize_credentials_for_a_desktop_application # noqa
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    client_secrets_file, scopes
-                )
-                creds = flow.run_local_server(port=0)
 
-            with open(token_file, "w") as token:
-                token.write(creds.to_json())
+def build_gmail_service(
+    credentials: Optional[Credentials] = None,
+    service_name: str = "gmail",
+    service_version: str = "v1",
+    use_domain_wide: bool = False,
+    delegated_user: Optional[str] = None,
+    service_account_file: Optional[str] = None,
+    scopes: Optional[List[str]] = None,
+) -> Resource:
+    """Build a Gmail service."""
+    if use_domain_wide:
+        scopes = scopes or DEFAULT_SERVICE_SCOPES
+    else:
+        scopes = scopes or DEFAULT_SCOPES
 
-        return creds
+    credentials = credentials or get_google_credentials(
+        scopes=scopes,
+        use_domain_wide=use_domain_wide,
+        delegated_user=delegated_user,
+        service_account_file=service_account_file,
+    )
+    builder = import_googleapiclient_resource_builder()
+    return builder(service_name, service_version, credentials=credentials)
 
 
 def build_resource_service(
@@ -136,15 +86,22 @@ def build_resource_service(
     service_account_file: Optional[str] = None,
     scopes: Optional[List[str]] = None,
 ) -> Resource:
-    """Build a Gmail service."""
-    credentials = credentials or get_gmail_credentials(
+    """Build a Gmail resource service."""
+    warnings.warn(
+        "build_resource_service is deprecated and will be removed in a future version."
+        "Use build_gmail_service instead.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    return build_gmail_service(
+        credentials=credentials,
+        service_name=service_name,
+        service_version=service_version,
         use_domain_wide=use_domain_wide,
         delegated_user=delegated_user,
         service_account_file=service_account_file,
         scopes=scopes,
     )
-    builder = import_googleapiclient_resource_builder()
-    return builder(service_name, service_version, credentials=credentials)
 
 
 def clean_email_body(body: str) -> str:
