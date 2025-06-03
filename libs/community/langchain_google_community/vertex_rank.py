84c42daa8878c2a8561c1644328a3c895ba06821
Matthias Uhlig <126894819+DRXD1000@users.noreply.github.com>
2025-06-03 15:25:08 +0200

    Attribute description

--- libs/community/langchain_google_community/vertex_rank.py
+++ libs/community/langchain_google_community/vertex_rank.py
@@ -52,6 +52,7 @@ class VertexAIRank(BaseDocumentCompressor):
         credentials (Optional[Credentials]): Google Cloud credentials object.
         credentials_path (Optional[str]): Path to the Google Cloud service
         account credentials file.
+        timeout (Optional[int]): Timeout for API calls in seconds.
     """
 
     project_id: str = Field(default=None)  # type: ignore
