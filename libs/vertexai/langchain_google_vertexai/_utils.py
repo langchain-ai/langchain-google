76e527ff65c31dcaf1260549755c5d73a6ef370e
Wilson Li <windkithk@google.com>
2025-07-29 18:14:38 +0900

    Update for lint.

--- libs/vertexai/langchain_google_vertexai/_utils.py
+++ libs/vertexai/langchain_google_vertexai/_utils.py
@@ -157,7 +157,8 @@ def get_generation_info(
                 "severity": rating.severity.name,
                 "severity_score": rating.severity_score,
             }
-            # Image generation models sometime return ratings that are not included in the proto.
+            # Image generation models sometime return ratings that are not
+            # included in the proto.
             for rating in candidate.safety_ratings
             if hasattr(rating.category, "name")
         ],
