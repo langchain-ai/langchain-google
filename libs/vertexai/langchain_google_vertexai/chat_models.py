e950f2960733188f6cd9ebf8834df7f917926ebf
Damian Deza <161374850+DamianDeza@users.noreply.github.com>
2025-08-04 12:45:22 -0300

    vertexai/genai: Added missing chunk to on_llm_new_token to _stream_gemini (#1058)

--- libs/vertexai/langchain_google_vertexai/chat_models.py
+++ libs/vertexai/langchain_google_vertexai/chat_models.py
@@ -2029,7 +2029,7 @@ class ChatVertexAI(_VertexAICommon, BaseChatModel):
                 response_chunk, prev_total_usage=total_lc_usage
             )
             if run_manager and isinstance(chunk.message.content, str):
-                run_manager.on_llm_new_token(chunk.message.content)
+                run_manager.on_llm_new_token(chunk.message.content, chunk=chunk)
             yield chunk
 
     async def _astream(
@@ -2057,7 +2057,7 @@ class ChatVertexAI(_VertexAICommon, BaseChatModel):
                 response_chunk, prev_total_usage=total_lc_usage
             )
             if run_manager and isinstance(chunk.message.content, str):
-                await run_manager.on_llm_new_token(chunk.message.content)
+                await run_manager.on_llm_new_token(chunk.message.content, chunk=chunk)
             yield chunk
 
     def with_structured_output(
