3260909a0262695a57c108703a84d435950d316e
Mason Daugherty <github@mdrxy.com>
2025-08-27 13:18:05 -0400

    warnings

--- libs/genai/langchain_google_genai/llms.py
+++ libs/genai/langchain_google_genai/llms.py
@@ -41,7 +41,7 @@ class GoogleGenerativeAI(_BaseGoogleGenerativeAI, BaseLLM):
         """Needed for arg validation."""
         # Get all valid field names, including aliases
         valid_fields = set()
-        for field_name, field_info in self.model_fields.items():
+        for field_name, field_info in self.__class__.model_fields.items():
             valid_fields.add(field_name)
             if hasattr(field_info, "alias") and field_info.alias is not None:
                 valid_fields.add(field_info.alias)
