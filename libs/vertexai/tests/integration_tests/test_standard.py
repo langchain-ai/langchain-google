1ceb9219a965caff1df07e55d4f0cfcce843d241
Chester Curme <chester.curme@gmail.com>
2025-09-11 15:26:42 -0400

    add missing import

--- libs/vertexai/tests/integration_tests/test_standard.py
+++ libs/vertexai/tests/integration_tests/test_standard.py
@@ -2,6 +2,7 @@
 
 from typing import Type
 
+import pytest
 from langchain_core.language_models import BaseChatModel
 from langchain_core.rate_limiters import InMemoryRateLimiter
 from langchain_tests.integration_tests import ChatModelIntegrationTests
