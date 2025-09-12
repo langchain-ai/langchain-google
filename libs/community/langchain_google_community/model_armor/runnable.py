913cc91dead0cd4c3834cf400d871e6486b33e05
Harsh Nasit <harsh.nasit@crestdata.ai>
2025-09-12 19:31:58 +0530

    docs: move Model Armor prerequisites from README to docstring

--- libs/community/langchain_google_community/model_armor/runnable.py
+++ libs/community/langchain_google_community/model_armor/runnable.py
@@ -1,6 +1,35 @@
 """
 Langchain Runnables to screen user prompt and/or model response using Google
 Cloud Model Armor.
+
+Prerequisites
+-------------
+
+Before using Model Armor Runnables, ensure the following steps are completed:
+
+- Select or create a Google Cloud Platform project.
+  - You can do this at: https://console.cloud.google.com/project
+
+- Enable billing for your project.
+  - Instructions: https://cloud.google.com/billing/docs/how-to/modify-project#enable_billing_for_a_project
+
+- Enable the Model Armor API in your GCP project.
+  - See: https://cloud.google.com/security-command-center/docs/get-started-model-armor
+
+- Grant the `modelarmor.user` IAM role to any user or service account that will use
+    the Model Armor runnables.
+
+- Authentication:
+  - Your application or environment must be authenticated and have the necessary
+    permissions to access the Model Armor service.
+  - You can authenticate using several methods described here: https://googleapis.dev/python/google-api-core/latest/auth.html
+
+- Create Model Armor Template:
+  - You must create a Model Armor template for prompt and response sanitization. You may
+    use a single template for both, or separate templates as needed.
+  - Refer to the guide: Create and manage Model Armor templates (https://cloud.google.com/security-command-center/docs/get-started-model-armor)
+  - The Template IDs must be provided when initializing the respective runnables.
+  - To manage Model Armor templates, the `modelarmor.admin` IAM role is required.
 """
 
 import logging
