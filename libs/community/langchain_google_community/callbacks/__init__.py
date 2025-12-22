c4a70145df8ce187909b7951468e1c94776f6b21
Huan Chen <huanc@google.com>
2025-12-22 11:46:51 +0000

    feat(google-community): Add BigQuery Callbacks (Sync and Async)

---
+++ libs/community/langchain_google_community/callbacks/__init__.py
@@ -0,0 +1,38 @@
+"""**Callback handlers** allow listening to events in LangChain.
+
+**Class hierarchy:**
+
+.. code-block::
+
+    BaseCallbackHandler --> <name>CallbackHandler  # Example: AimCallbackHandler
+"""
+
+import importlib
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from langchain_google_community.callbacks.bigquery_callback import (
+        AsyncBigQueryCallbackHandler,
+        BigQueryCallbackHandler,
+    )
+
+
+_module_lookup = {
+    "AsyncBigQueryCallbackHandler": (
+        "langchain_google_community.callbacks.bigquery_callback"
+    ),
+    "BigQueryCallbackHandler": "langchain_google_community.callbacks.bigquery_callback",
+}
+
+
+def __getattr__(name: str) -> Any:
+    if name in _module_lookup:
+        module = importlib.import_module(_module_lookup[name])
+        return getattr(module, name)
+    raise AttributeError(f"module {__name__} has no attribute {name}")
+
+
+__all__ = [
+    "AsyncBigQueryCallbackHandler",
+    "BigQueryCallbackHandler",
+]
